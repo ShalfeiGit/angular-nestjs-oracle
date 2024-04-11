58210c6ac27f5105b108afddd68c32eab55253ab
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2024-04-12 06:43:09 +0700

    add angular

---
+++ backend/src/ormconfig.ts
@@ -0,0 +1,14 @@
+﻿import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5434,
+  username: 'dbUser',
+  password: 'dbPassword',
+  database: 'dbAngular',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*.ts'],
+};
+
+export const dataSource = new DataSource(dataSourceOptions);
